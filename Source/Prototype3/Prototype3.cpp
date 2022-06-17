36a58b2aa069788f4ae81beeeaea509233a33cd5
David Barrett <dlbarret@usc.edu>
2022-06-16 17:20:47 -0700

    Project 3D Camera Template Init
    Pushing Unreal 3D camera template default project state.

---
+++ Source/Prototype3/Prototype3.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Prototype3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prototype3, "Prototype3" );
+
+DEFINE_LOG_CATEGORY(LogPrototype3)
+ 
